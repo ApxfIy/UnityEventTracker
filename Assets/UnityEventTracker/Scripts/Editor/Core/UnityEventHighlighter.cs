160ceb0a7af5698e360bf05aa5c6b552d84a3ad0
Alexsandr Krivobokov <apxfly1@gmail.com>
2022-03-08 14:50:54 +1000

    Update UnityEventHighlighter.cs
    1) Add support for nested events
    2) Remove ObjectData struct and Validate method

--- Assets/UnityEventTracker/Scripts/Editor/Core/UnityEventHighlighter.cs
+++ Assets/UnityEventTracker/Scripts/Editor/Core/UnityEventHighlighter.cs
@@ -14,22 +14,36 @@ namespace UnityEventTracker
     internal static class UnityEventHighlighter
     {
         private static readonly Type BaseUnityEventType = typeof(UnityEventBase);
-        private static readonly FieldInfo PersistentCallsField = BaseUnityEventType.GetField("m_PersistentCalls", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        private static readonly FieldInfo PersistentCallsField =
+            BaseUnityEventType.GetField("m_PersistentCalls", BindingFlags.Instance | BindingFlags.NonPublic);
+
         private static readonly Type PersistentCallGroupType = PersistentCallsField.FieldType;
-        private static readonly MethodInfo GetListener = PersistentCallGroupType.GetMethod("GetListener", BindingFlags.Instance | BindingFlags.Public);
+
+        private static readonly MethodInfo GetListener =
+            PersistentCallGroupType.GetMethod("GetListener", BindingFlags.Instance | BindingFlags.Public);
+
         private static readonly Type PersistentCallType = GetListener.ReturnType;
-        private static readonly FieldInfo Mode = PersistentCallType.GetField("m_Mode", BindingFlags.Instance | BindingFlags.NonPublic);
-        private static readonly FieldInfo Arguments = PersistentCallType.GetField("m_Arguments", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        private static readonly FieldInfo Mode =
+            PersistentCallType.GetField("m_Mode", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        private static readonly FieldInfo Arguments =
+            PersistentCallType.GetField("m_Arguments", BindingFlags.Instance | BindingFlags.NonPublic);
+
         private static readonly Type ArgumentsType = Arguments.FieldType;
-        private static readonly FieldInfo Argument = ArgumentsType.GetField("m_ObjectArgumentAssemblyTypeName", BindingFlags.Instance | BindingFlags.NonPublic);
-        private static readonly Type ObjectType = typeof(object);
+
+        private static readonly FieldInfo Argument = ArgumentsType.GetField("m_ObjectArgumentAssemblyTypeName",
+            BindingFlags.Instance | BindingFlags.NonPublic);
+
+        private static readonly Type ObjectType         = typeof(object);
         private static readonly Type SerializeFieldType = typeof(SerializeField);
 
-        private static readonly Dictionary<int, Item> ProceedObjects = new Dictionary<int, Item>();
-        private static readonly HashSet<int> ProceedObjectsWithoutEvents = new HashSet<int>();
-        private static int[] _previousSelection;
-        private static readonly List<int> Deselected = new List<int>();
-        
+        private static readonly Dictionary<int, Item> ProceedObjects              = new Dictionary<int, Item>();
+        private static readonly HashSet<int>          ProceedObjectsWithoutEvents = new HashSet<int>();
+        private static          int[]                 _previousSelection;
+        private static readonly List<int>             Deselected = new List<int>();
+
         private static readonly UnityEventTrackerSettings Settings;
 
         static UnityEventHighlighter()
@@ -49,9 +63,7 @@ namespace UnityEventTracker
                 if (!Settings.IsHighlightingEnabled) return;
 
                 foreach (var item in ProceedObjects.Values.Where(item => !item.IsValid))
-                {
                     item.HierarchyItem.BackgroundColor = color;
-                }
             };
 
             Settings.ValidCallColor.OnChange += color =>
@@ -59,9 +71,7 @@ namespace UnityEventTracker
                 if (!Settings.IsHighlightingEnabled) return;
 
                 foreach (var item in ProceedObjects.Values.Where(item => item.IsValid))
-                {
                     item.HierarchyItem.BackgroundColor = color;
-                }
             };
 
             if (!Settings.IsHighlightingEnabled) return;
@@ -80,7 +90,7 @@ namespace UnityEventTracker
             Selection.selectionChanged += SelectionChanged;
 
             _previousSelection = Selection.instanceIDs;
-             
+
             EditorApplication.RepaintHierarchyWindow();
         }
 
@@ -111,105 +121,80 @@ namespace UnityEventTracker
 
         private static void HandleHierarchyWindowItemOnGUI(int instanceId, Rect rect)
         {
-            if (ProceedObjects.ContainsKey(instanceId))
+            try
             {
-                var item = ProceedObjects[instanceId];
-
-                if (Deselected.Contains(instanceId))
+                if (ProceedObjects.ContainsKey(instanceId))
                 {
-                    var go = item.HierarchyItem.GameObject;
+                    var item = ProceedObjects[instanceId];
 
-                    // If gameObject was destroyed
+                    if (Deselected.Contains(instanceId))
+                    {
+                        var go = item.HierarchyItem.GameObject;
+
+                        // If gameObject was destroyed
+                        if (go == null) return;
+
+                        var isValidOptional = ValidateUnityEvents(go);
+
+                        if (!isValidOptional.HasValue(out var isValid))
+                        {
+                            ProceedObjects.Remove(instanceId);
+                            ProceedObjectsWithoutEvents.Add(instanceId);
+                            return;
+                        }
+
+                        item.HierarchyItem.BackgroundColor = isValid ? Settings.ValidCallColor : Settings.InvalidCallColor;
+                    }
+
+                    item.HierarchyItem.UpdateRect(rect);
+                    item.HierarchyItem.Draw();
+                    return;
+                }
+
+                if (ProceedObjectsWithoutEvents.Contains(instanceId))
+                    if (!Deselected.Remove(instanceId))
+                        return;
+
+                {
+                    var instance = EditorUtility.InstanceIDToObject(instanceId);
+
+                    if (instance == null) return;
+
+                    var go = instance as GameObject;
+
                     if (go == null) return;
 
-                    var isValidOptional = Validate(go);
+                    var isValidOptional = ValidateUnityEvents(go);
 
                     if (!isValidOptional.HasValue(out var isValid))
                     {
-                        ProceedObjects.Remove(instanceId);
                         ProceedObjectsWithoutEvents.Add(instanceId);
                         return;
                     }
 
-                    item.HierarchyItem.BackgroundColor = isValid ? Settings.ValidCallColor : Settings.InvalidCallColor;
+                    var hierarchyItem = new HierarchyItem(instanceId, rect, go)
+                    {
+                        BackgroundColor = isValid ? Settings.ValidCallColor : Settings.InvalidCallColor
+                    };
+                    hierarchyItem.Draw();
+
+                    ProceedObjects.Add(instanceId, new Item(hierarchyItem, isValid));
                 }
-
-                item.HierarchyItem.UpdateRect(rect);
-                item.HierarchyItem.Draw();
-                return;
             }
-
-            if (ProceedObjectsWithoutEvents.Contains(instanceId))
+            catch
             {
-                if (!Deselected.Remove(instanceId)) return;
-            }
-
-            {
-                var instance = EditorUtility.InstanceIDToObject(instanceId);
-
-                if (instance == null) return;
-
-                var go = instance as GameObject;
-
-                if (go == null) return;
-
-                var isValidOptional = Validate(go);
-
-                if (!isValidOptional.HasValue(out var isValid))
-                {
-                    ProceedObjectsWithoutEvents.Add(instanceId);
-                    return;
-                }
-
-                var hierarchyItem = new HierarchyItem(instanceId, rect, go)
-                {
-                    BackgroundColor = isValid ? Settings.ValidCallColor : Settings.InvalidCallColor
-                };
-                hierarchyItem.Draw();
-
-                ProceedObjects.Add(instanceId, new Item(hierarchyItem, isValid));
+                // Ignores
             }
         }
 
-        private static Optional<bool> Validate(GameObject gameObject)
+        private static Optional<bool> ValidateUnityEvents(GameObject gameObject)
         {
-            var components = gameObject.GetComponents<MonoBehaviour>().Where(c => c != null).ToArray();
-
-            var data = new ObjectData[components.Length];
-
-            for (var i = 0; i < components.Length; i++)
-            {
-                var monoBehaviour = components[i];
-                var type = monoBehaviour.GetType();
-                var fields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-
-                data[i] = new ObjectData(monoBehaviour, fields);
-            }
-
-            return ValidateUnityEvents(data);
-        }
-
-        private static Optional<bool> ValidateUnityEvents(IEnumerable<ObjectData> data)
-        {
+            var components  = gameObject.GetComponents<MonoBehaviour>().Where(c => c != null).ToArray();
             var unityEvents = new List<Func<UnityEventBase>>();
 
-            foreach (var objectData in data)
-            {
-                var monoBehaviour = objectData.MonoBehaviour;
-                var fields = objectData.Fields;
+            foreach (var component in components) unityEvents.AddRange(GetEvents(component));
 
-                for (var i = 0; i < fields.Length; i++)
-                {
-                    var fieldInfo = fields[i];
-
-                    if (!BaseUnityEventType.IsAssignableFrom(fieldInfo.FieldType)) continue;
-                    if (!fieldInfo.IsPublic && !Attribute.IsDefined(fieldInfo, SerializeFieldType)) continue;
-
-                    unityEvents.Add(() => (UnityEventBase) fieldInfo.GetValue(monoBehaviour));
-                }
-            }
-
-            if (unityEvents.Count == 0) 
+            if (unityEvents.Count == 0)
                 return Optional<bool>.FromNone();
 
             var isValid = true;
@@ -221,7 +206,7 @@ namespace UnityEventTracker
                 if (e == null) continue;
 
                 var persistentCalls = PersistentCallsField.GetValue(e);
-                var count = e.GetPersistentEventCount();
+                var count           = e.GetPersistentEventCount();
 
                 if (count == 0) continue;
 
@@ -232,14 +217,14 @@ namespace UnityEventTracker
                     var mode = (PersistentListenerMode) Mode.GetValue(call);
 
                     var methodName = e.GetPersistentMethodName(i);
-                    var target = e.GetPersistentTarget(i);
+                    var target     = e.GetPersistentTarget(i);
 
                     var arguments = Arguments.GetValue(call);
 
                     if (arguments == null) continue;
 
                     var desiredArgTypeName = (string) Argument.GetValue(arguments);
-                    var desiredType = ObjectType;
+                    var desiredType        = ObjectType;
 
                     if (!string.IsNullOrEmpty(desiredArgTypeName))
                         desiredType = Type.GetType(desiredArgTypeName, false) ?? ObjectType;
@@ -256,27 +241,84 @@ namespace UnityEventTracker
             return Optional<bool>.FromSome(isValid);
         }
 
-        private struct ObjectData
+        private static IEnumerable<Func<UnityEventBase>> GetEvents(MonoBehaviour target)
         {
-            public MonoBehaviour MonoBehaviour { get; }
-            public FieldInfo[] Fields { get; }
+            var fields = target
+                         .GetType().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
 
-            public ObjectData(MonoBehaviour monoBehaviour, FieldInfo[] fields)
+            var result = new List<Func<UnityEventBase>>();
+
+            foreach (var fieldInfo in fields)
             {
-                MonoBehaviour = monoBehaviour;
-                Fields = fields;
+                if (!fieldInfo.IsPublic && !Attribute.IsDefined(fieldInfo, typeof(SerializeField))) continue;
+
+                if (BaseUnityEventType.IsAssignableFrom(fieldInfo.FieldType))
+                {
+                    var info = fieldInfo;
+                    result.Add(() => (UnityEventBase) info.GetValue(target));
+                    continue;
+                }
+
+                if ((fieldInfo.FieldType.Attributes & TypeAttributes.Serializable) == 0) continue;
+
+                result.AddRange(GetEventsRecursive(fieldInfo, fieldInfo.GetValue(target)));
             }
+
+            return result;
+        }
+
+        private static IEnumerable<Func<UnityEventBase>> GetEventsRecursive(FieldInfo field, object parent,
+            Dictionary<Type, int>                                                     visited = null)
+        {
+            if (parent == null)
+                return new List<Func<UnityEventBase>>();
+
+            var fields = field.FieldType
+                              .GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            visited ??= new Dictionary<Type, int> {{field.FieldType, 1}};
+
+            var result = new List<Func<UnityEventBase>>();
+
+            foreach (var fieldInfo in fields)
+            {
+                if (!fieldInfo.IsPublic && !Attribute.IsDefined(fieldInfo, typeof(SerializeField))) continue;
+
+                if (BaseUnityEventType.IsAssignableFrom(fieldInfo.FieldType))
+                {
+                    var info = fieldInfo;
+                    result.Add(() => (UnityEventBase) info.GetValue(parent));
+                    continue;
+                }
+
+                if ((fieldInfo.FieldType.Attributes & TypeAttributes.Serializable) == 0) continue;
+
+                if (visited.ContainsKey(fieldInfo.FieldType))
+                {
+                    if (visited[fieldInfo.FieldType] >= 10)
+                        continue;
+
+                    visited[fieldInfo.FieldType]++;
+                }
+                else
+                {
+                    visited.Add(fieldInfo.FieldType, 1);
+                }
+
+                result.AddRange(GetEventsRecursive(fieldInfo, fieldInfo.GetValue(parent), visited));
+            }
+
+            return result;
         }
 
         private class Item
         {
             public HierarchyItem HierarchyItem { get; }
-            public bool IsValid { get; }
+            public bool          IsValid       { get; }
 
             public Item(HierarchyItem hierarchyItem, bool isValid)
             {
                 HierarchyItem = hierarchyItem;
-                IsValid = isValid;
+                IsValid       = isValid;
             }
         }
     }
